355dc3c94ad129aa0e3012d24be1918cee6fa3e4
USER <aissa04@campus.haifa.ac.il>
2025-05-24 10:59:13 +0300

    arkan push

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/App.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/App.java
@@ -2,20 +2,6 @@ package il.cshaifasweng.OCSFMediatorExample.server;
 
 import java.io.IOException;
 
-
-import java.util.List;
-import java.util.Random;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-
-import il.cshaifasweng.OCSFMediatorExample.entities.Flower;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 /**
  * Hello world!
  *
