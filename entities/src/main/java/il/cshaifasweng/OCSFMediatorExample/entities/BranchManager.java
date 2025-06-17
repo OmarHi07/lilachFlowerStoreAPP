1e62d6ea39b66ef31819bac7eec0be0b29380f85
kabos <kabosharke@gmail.com>
2025-06-17 19:55:59 +0300

    tables

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/BranchManager.java
@@ -0,0 +1,23 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@SuppressWarnings("serial")
+@Entity
+public class BranchManager extends Employee implements Serializable{
+    @OneToOne(mappedBy = "manager")
+    private Branch Branch;
+    public BranchManager() {super();}
+    public BranchManager(String name , String username, String password, Branch branch) {
+        super(name, username, password);
+        setBranch(Branch);
+    }
+    public Branch getBranch(){
+        return Branch;
+    }
+    public void setBranch(Branch branch){
+        this.Branch = branch;
+        Branch.setManager(this);
+    }
+}
---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/SystemAdmin.java
@@ -0,0 +1,19 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+
+@SuppressWarnings("serial")
+@Entity
+public class SystemAdmin extends Employee implements Serializable {
+    public SystemAdmin() {
+        super();
+    }
+
+    public SystemAdmin(String name, String username, String pass) {
+        super(name, username, pass);
+        this.permission = 5;
+    }
+
+}
